97f540e2133f10d8d6d294f6ba0430fed43337b7
hunter <104146303+solhosty@users.noreply.github.com>
2023-08-21 19:31:22 -0400

    new changes

--- main.ts
+++ main.ts
@@ -1,105 +1,54 @@
 import {
-    Connection,
-    Keypair,
-    LAMPORTS_PER_SOL,
-    NonceAccount,
-    NONCE_ACCOUNT_LENGTH,
-    SystemProgram,
-    Transaction,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  NonceAccount,
+  NONCE_ACCOUNT_LENGTH,
+  SystemProgram,
+  Transaction,
 } from "@solana/web3.js";
-import { encode, decode } from 'bs58';
-import { sendVersionedTx } from './utils'
-import fs from 'fs';
+import {
+  encodeAndWriteTransaction,
+  loadWallet,
+  readAndDecodeTransaction,
+} from "./utils";
 
-const RPC_URL = 'http://127.0.0.1:8899';
 const TRANSFER_AMOUNT = LAMPORTS_PER_SOL * 0.01;
+const NONCE_AUTH_KEYPAIR = loadWallet("./wallets/nonceAuth.json");
+const NONCE_KEYPAIR = Keypair.generate();
+const SENDER_KEYPAIR = loadWallet("./wallets/wallet.json");
+const CONNECTION = new Connection(
+  "https://devnet.helius-rpc.com/?api-key=<api-key>"
+);
+const WAIT_TIME = 120000;
 
-async function performSolanaTransfer(useNonce = false, waitTime = 120000) {
-    const nonceAuthKeypair = Keypair.generate();
-    const nonceKeypair = Keypair.generate();
-    const payer = Keypair.generate();
-    const connection = new Connection(RPC_URL);
-
-    // Helper Functions
-    const fetchNonceInfo = async () => {
-        const accountInfo = await connection.getAccountInfo(nonceKeypair.publicKey);
-        if (!accountInfo) throw new Error("No account info found");
-        return NonceAccount.fromAccountData(accountInfo.data);
-    };
-
-    const encodeAndWriteTransaction = async (tx: Transaction, filename: string, requireAllSignatures = true) => {
-        const serialisedTx = encode(tx.serialize({ requireAllSignatures }));
-        fs.writeFileSync(filename, serialisedTx);
-        return serialisedTx;
-    };
-
-    const readAndDecodeTransaction = async (filename: string) => {
-        const transactionData = fs.readFileSync(filename, 'utf-8');
-        const decodedData = decode(transactionData);
-        return Transaction.from(decodedData);
-    };
-
-    // Core logic
-    try {
-        // Step 1: Fund Accounts
-        const accountsToFund = [nonceAuthKeypair, payer];
-        const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
-        await Promise.all(
-            accountsToFund.map(account => connection.requestAirdrop(account.publicKey, LAMPORTS_PER_SOL))
-        );
-        await Promise.all(
-            accountsToFund.map(account => connection.confirmTransaction({ signature: account.publicKey.toBase58(), blockhash, lastValidBlockHeight }, 'finalized'))
-        );
-
-        // Step 2: Create the Nonce Account
-        const rent = await connection.getMinimumBalanceForRentExemption(NONCE_ACCOUNT_LENGTH);
-        const createNonceTx = [
-                SystemProgram.createAccount({
-                    fromPubkey: nonceAuthKeypair.publicKey,
-                    newAccountPubkey: nonceKeypair.publicKey,
-                    lamports: rent,
-                    space: NONCE_ACCOUNT_LENGTH,
-                    programId: SystemProgram.programId,
-                }),
-                SystemProgram.nonceInitialize({
-                    noncePubkey: nonceKeypair.publicKey,
-                    authorizedPubkey: nonceAuthKeypair.publicKey,
-                })
-        ]
-        const tx =  await sendVersionedTx(connection, createNonceTx, payer.publicKey, [nonceAuthKeypair, nonceKeypair]);
-
-        // Step 3: Create a Transaction
-        const destination = Keypair.generate();
-        const transaction = [
-            SystemProgram.transfer({
-                fromPubkey: payer.publicKey,
-                toPubkey: destination.publicKey,
-                lamports: TRANSFER_AMOUNT,
-            })
-        ]
-        if (useNonce) {
-            transaction.recentBlockhash = (await fetchNonceInfo()).nonce;
-        } else {
-            transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-        }
-        transaction.feePayer = payer.publicKey;
-        encodeAndWriteTransaction(transaction, './unsigned.json', false);
-
-        // Step 4: Sign the Transaction Offline
-        await new Promise(resolve => setTimeout(resolve, waitTime));
-        const unsignedTx = await readAndDecodeTransaction('./unsigned.json');
-        if (useNonce) {
-            unsignedTx.sign(nonceAuthKeypair, payer);
-        } else {
-            unsignedTx.sign(payer);
-        }        encodeAndWriteTransaction(unsignedTx, './signed.json');
-
-        // Step 5: Execute the Transaction
-        const signedTx = await readAndDecodeTransaction('./signed.json');
-        const txId = await sendVersionedTx(connection, unsignedTx, payer.publicKey, []);
-        console.log("Transaction ID:", txId);
-
-    } catch (error) {
-        console.error(error);
-    }
+async function sendTransaction() {
+  // Code goes here for the function
 }
+
+async function nonce() {
+  // Code goes here for the function
+}
+
+async function getNonce() {
+  // Code goes here for the function
+}
+
+async function createTx(nonce) {
+  // Code goes here for the function
+}
+
+async function signOffline(waitTime = WAIT_TIME) {
+  // Code goes here for the function
+}
+
+async function executeTx() {
+  // Code goes here for the function
+}
+
+async function fetchNonceInfo(retries = 3) {
+  // Code goes here for the function
+}
+
+sendTransaction();
+
--- utils.ts
+++ utils.ts
@@ -1,19 +1,26 @@
-import { Connection, TransactionInstruction, PublicKey, Signer, TransactionMessage, VersionedTransaction } from "@solana/web3.js";
+// utils.ts
+import { decode, encode } from 'bs58';
+import fs from 'fs';
+import { Transaction } from "@solana/web3.js";
+import { Keypair } from '@solana/web3.js';
 
-export async function sendVersionedTx(
-    connection: Connection,
-    instructions: TransactionInstruction[],
-    payer: PublicKey,
-    signers: Signer[]
-  ) {
-    let latestBlockhash = await connection.getLatestBlockhash();
-    const message = new TransactionMessage({
-      payerKey: payer,
-      recentBlockhash: latestBlockhash.blockhash,
-      instructions,
-    }).compileToLegacyMessage();
-    const transaction = new VersionedTransaction(message);
-    transaction.sign(signers);
-    const signature = await connection.sendTransaction(transaction);
-    return signature;
+export function encodeAndWriteTransaction(tx: Transaction, filename: string, requireAllSignatures = true): string {
+    const serialisedTx = encode(tx.serialize({ requireAllSignatures }));
+    fs.writeFileSync(filename, serialisedTx);
+    console.log(`      Tx written to ${filename}`);
+    return serialisedTx;
+}
+
+export function readAndDecodeTransaction(filename: string): Transaction {
+    const transactionData = fs.readFileSync(filename, 'utf-8');
+    const decodedData = decode(transactionData);
+    const transaction = Transaction.from(decodedData);
+    return transaction;
+}
+export function loadWallet(kFile: string): Keypair {
+    const fs = require("fs");
+    return Keypair.fromSecretKey(
+      new Uint8Array(JSON.parse(fs.readFileSync(kFile).toString()))
+    );
   }
+
