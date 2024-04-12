7824034295bae4ca747578a0ec27630ad0c17219
Nathan Galindo <nathan.zebedee@gmail.com>
2024-04-11 19:55:20 -0500

    revise imports

--- main.ts
+++ main.ts
@@ -1,26 +1,21 @@
-import {
-  Connection,
-  Keypair,
-  LAMPORTS_PER_SOL,
-  NonceAccount,
-  NONCE_ACCOUNT_LENGTH,
-  SystemProgram,
-  Transaction,
-} from "@solana/web3.js";
-import {
-  encodeAndWriteTransaction,
-  loadWallet,
-  readAndDecodeTransaction,
-} from "./utils";
+import * as web3 from "@solana/web3.js";
+import { Helius } from "helius-sdk";
 
-const TRANSFER_AMOUNT = LAMPORTS_PER_SOL * 0.01;
+import { encodeAndWriteTransaction, loadWallet, readAndDecodeTransaction } from "./utils";
+
+// The URL of the default Solana cluster (devnet)
+const { connection } = new Helius("https://devnet.helius-rpc.com/?api-key=<api-key>");
+// The number of SOL we will transfer in our sample transaction
+const TRANSFER_AMNT = web3.LAMPORTS_PER_SOL * 0.01;
+// The amount of time before we send the Durable Nonce transaction
+const DELAY = 120_000;
+
+// The keypair for the nonce account
+const NONCE_KEYPAIR = web3.Keypair.generate();
+// The keypair for the nonce authority account
 const NONCE_AUTH_KEYPAIR = loadWallet("./wallets/nonceAuth.json");
-const NONCE_KEYPAIR = Keypair.generate();
-const SENDER_KEYPAIR = loadWallet("./wallets/wallet.json");
-const CONNECTION = new Connection(
-  "https://devnet.helius-rpc.com/?api-key=<api-key>"
-);
-const WAIT_TIME = 120000;
+// The keypair for the sender account
+const SENDER_KEYPAIR = loadWallet("./wallets/sender.json");
 
 async function sendTransaction() {
   // Code goes here for the function
